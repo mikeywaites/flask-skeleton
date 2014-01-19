ccd43a51904ebb4793ad655893b30f8cee61c085
Mike Waites <mikey.waites@gmail.com>
2014-01-19 21:58:27 +0000

    expose home view on example blueprint at '/'

--- {{PROJECT_NAME}}/example/views.py
+++ {{PROJECT_NAME}}/example/views.py
@@ -2,9 +2,13 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template
 
-mod = Blueprint('example', __name__, url_prefix='/example')
+#: Specify each model defined in your models.py for each app here. This will
+#: allow SQA to pick them up when the application is initialized
+from .models import *
+
+mod = Blueprint('example', __name__)
 
 
-@mod.route('')
+@mod.route('/')
 def home():
     return render_template("example/home.html")
