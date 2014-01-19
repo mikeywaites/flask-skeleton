304cd568553d05139b176b27085074ceefe8baa4
Mike Waites <mikey.waites@gmail.com>
2014-01-19 21:58:05 +0000

    document models.py showing example model def

--- {{PROJECT_NAME}}/example/models.py
+++ {{PROJECT_NAME}}/example/models.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+"""
+    An example models.py for use with {{PROJECT_NAME}}
+
+    Useage::
+
+        from {{PROJECT_NAME}}.database import db
+
+        class FooModel(db.Model):
+
+            __tablename__ = 'foos'
+
+            id = db.Column(db.Integer, primary=True)
+            name = db.Column(db.String(30), nullable=False)
+"""
+
+from {{PROJECT_NAME}}.database import db
